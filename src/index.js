440b9217c627a76800f8c93c86ae0e27b93af865
borilooa <alba.c.borilo.ondo@avanade.com>
2021-11-25 12:32:51 +0100

    +Added registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {  BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 import App from "./App";
 import { createStore } from "redux"
 import { Provider } from "react-redux"
@@ -10,7 +11,6 @@ import reducer from "./reducers"
   const store = createStore( reducer )
 
 ReactDOM.render(<Provider store={store}>
-                    <Router>
                         <App />
-                        </Router>
                 </Provider>, document.getElementById("root"));
+registerServiceWorker();
